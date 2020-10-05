fe1bbc152fc0d91f4b87b9636fb42d0630edd5ef
Sammers21 <titantins@gmail.com>
2020-10-05 23:12:14 +0300

    initial commit

---
+++ src/main/java/io/rcapp/Main.java
@@ -0,0 +1,15 @@
+package io.rcapp;
+
+import io.vertx.core.Vertx;
+
+public class Main {
+    public static void main(String[] args) {
+        final Vertx vertx = Vertx.vertx();
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                .putHeader("content-type", "text/plain")
+                .end("Hello world!");
+        }).listen(8080);
+        System.out.println("Started at port 8080");
+    }
+}
