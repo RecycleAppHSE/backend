85625c9c0845cd45cc879f28cddf1d4dda63400e
Sammers21 <titantins@gmail.com>
2020-11-02 03:15:07 +0300

    improve ddl

--- src/main/java/io/rcapp/imprt/Importer.java
+++ src/main/java/io/rcapp/imprt/Importer.java
@@ -4,6 +4,7 @@ import io.rcapp.Main;
 import io.vertx.core.json.JsonObject;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
+import io.vertx.reactivex.pgclient.PgPool;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,7 +19,7 @@ public class Importer {
     Main.useVertxSchedulers(vertx);
     final List<JsonObject> rcmap =
         new RecycleMapRu(WebClient.create(vertx)).download().toList().blockingGet();
-    log.info("dataset size: {}", rcmap.size());
+    final PgPool pool = PgPool.pool(System.getenv("DB_URI"));
     System.exit(0);
   }
 }
--- src/main/resources/postgres/ddl.sql
+++ src/main/resources/postgres/ddl.sql
@@ -1,5 +1,27 @@
-create table rc_user(
-    id serial primary key,
-    name text,
+create table rc_user
+(
+    id        serial primary key,
+    name      text,
     photo_url text
+);
+
+create table collection_point
+(
+    id           serial primary key,
+    gid          int unique not null,
+    feature_name text,
+    plastic      bool       not null,
+    glass        bool       not null,
+    paper        bool       not null,
+    metal        bool       not null,
+    tetra_pack   bool       not null,
+    batteries    bool       not null,
+    light_bulbs  bool       not null,
+    clothes      bool       not null,
+    appliances   boolean    not null,
+    toxic        bool       not null,
+    other        bool       not null,
+    caps         bool       not null,
+    tires        bool       not null,
+    geom         geometry
 );
