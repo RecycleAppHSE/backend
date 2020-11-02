adfb0487efa4660e01247ab1a9722b91f6f36c5d
Sammers21 <titantins@gmail.com>
2020-11-02 19:19:30 +0300

    update ddl schema + gitignore

--- src/main/resources/postgres/ddl.sql
+++ src/main/resources/postgres/ddl.sql
@@ -1,4 +1,5 @@
 create database rcapp;
+
 create table rc_user
 (
     id        serial primary key,
@@ -7,11 +8,12 @@ create table rc_user
 );
 
 create extension postgis;
+
 create table collection_point
 (
     id           serial primary key,
     gid          int unique not null,
-    feature_name text,
+    feature_name text       not null,
     plastic      bool       not null,
     glass        bool       not null,
     paper        bool       not null,
@@ -20,10 +22,10 @@ create table collection_point
     batteries    bool       not null,
     light_bulbs  bool       not null,
     clothes      bool       not null,
-    appliances   boolean    not null,
+    appliances   bool       not null,
     toxic        bool       not null,
     other        bool       not null,
     caps         bool       not null,
     tires        bool       not null,
-    geom         geometry
+    geom         geometry   not null
 );
