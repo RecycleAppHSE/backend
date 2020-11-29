a7be1e653edb099f3a139f1e2119f6328650faa4
Sammers21 <titantins@gmail.com>
2020-11-29 22:05:39 +0300

    schema update

--- src/main/resources/postgres/ddl.sql
+++ src/main/resources/postgres/ddl.sql
@@ -32,7 +32,7 @@ create table collection_point
     -- additional fields
     phone_number      text,
     web_site          text,
-    works             text default 'works_fine',               -- can be broken, would_not_work, works_fine
+    works             text default 'works_fine',               -- может быть broken, would_not_work, works_fine
     last_updated      timestamp without time zone
                            default (now() at time zone 'utc'), -- utc timestamp
     schedule          text,                                    -- example: '09:00-17:00'
@@ -41,3 +41,32 @@ create table collection_point
 );
 
 CREATE INDEX full_text_by_address ON collection_point USING GIN (to_tsvector('english', address));
+
+CREATE TYPE collection_point_field_for_change AS ENUM (
+    'recycle', -- Поле состоящее из столбцов plastic, glass, metal, ... tires.
+    'works'    -- Поле works в таблице collection_point
+);
+
+CREATE TYPE correction_status AS ENUM (
+    'applied',    -- изменение не было применено
+    'rejected',   -- изменение было отклонено
+    'in-progress' -- коррекция в находится в процессе голосования
+);
+
+create table correction
+(
+    rc_user_id          integer                           not null,
+    collection_point_id integer                           not null,
+    field               collection_point_field_for_change not null, -- поле в структуре collection_point.
+    change_from         text                              not null, -- Пример: ["paper", "toxic"]
+    change_to           text                              not null, -- Пример: ["light_bulbs", "glass"]
+    status              correction_status                 not null, -- Статус коррекции
+    submit_time         timestamp without time zone
+        default (now() at time zone 'utc'),                         -- utc timestamp
+    constraint fk_correction_rc_user_id foreign key (rc_user_id) references rc_user (id),
+    constraint fk_correction_collection_point_id foreign key (collection_point_id) references collection_point (id)
+);
+
+create index correction_by_user_id on correction(rc_user_id);
+create index correction_by_collection_point_id on correction(rc_user_id);
+create index correction_ftype_and_collection_point_id on correction(collection_point_id, field);
