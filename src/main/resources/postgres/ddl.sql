a5981356beb3f025c6f9d0731e7bdb3af95e8b8a
Sammers21 <titantins@gmail.com>
2020-11-29 22:20:32 +0300

    schema update

--- src/main/resources/postgres/ddl.sql
+++ src/main/resources/postgres/ddl.sql
@@ -32,7 +32,7 @@ create table collection_point
     -- additional fields
     phone_number      text,
     web_site          text,
-    works             text default 'works_fine',               -- может быть broken, would_not_work, works_fine
+    works             text default 'works_fine',               -- cab be broken, would_not_work, works_fine
     last_updated      timestamp without time zone
                            default (now() at time zone 'utc'), -- utc timestamp
     schedule          text,                                    -- example: '09:00-17:00'
@@ -43,26 +43,29 @@ create table collection_point
 CREATE INDEX full_text_by_address ON collection_point USING GIN (to_tsvector('english', address));
 
 CREATE TYPE collection_point_field_for_change AS ENUM (
-    'recycle', -- Поле состоящее из столбцов plastic, glass, metal, ... tires.
-    'works'    -- Поле works в таблице collection_point
+    'recycle', -- Field composed of fields plastic, glass, metal, ... tires.
+    'works'    -- Field works at collection_point
 );
 
 CREATE TYPE correction_status AS ENUM (
-    'applied',    -- изменение не было применено
-    'rejected',   -- изменение было отклонено
-    'in-progress' -- коррекция в находится в процессе голосования
+    'applied',    -- correction has been applied
+    'rejected',   -- correction has been rejected
+    'in-progress' -- correction process is in progress
 );
 
 create table correction
 (
+    id                  serial primary key,
     rc_user_id          integer                           not null,
     collection_point_id integer                           not null,
-    field               collection_point_field_for_change not null, -- поле в структуре collection_point.
-    change_from         text                              not null, -- Пример: ["paper", "toxic"]
-    change_to           text                              not null, -- Пример: ["light_bulbs", "glass"]
-    status              correction_status                 not null, -- Статус коррекции
+    field               collection_point_field_for_change not null, -- Field at collection_point.
+    change_from         text                              not null, -- Example: ["paper", "toxic"]
+    change_to           text                              not null, -- Example: ["light_bulbs", "glass"]
+    status              correction_status                 not null, -- Correction status
     submit_time         timestamp without time zone
         default (now() at time zone 'utc'),                         -- utc timestamp
+    like_count          integer                           not null,
+    dislike_count       integer                           not null,
     constraint fk_correction_rc_user_id foreign key (rc_user_id) references rc_user (id),
     constraint fk_correction_collection_point_id foreign key (collection_point_id) references collection_point (id)
 );
@@ -70,3 +73,13 @@ create table correction
 create index correction_by_user_id on correction(rc_user_id);
 create index correction_by_collection_point_id on correction(rc_user_id);
 create index correction_ftype_and_collection_point_id on correction(collection_point_id, field);
+
+create table correction_like
+(
+    rc_user_id    integer not null,
+    correction_id integer not null,
+    is_like       bool    not null, -- true if like, false if dislike.
+    constraint fk_correction_like_rc_user_id foreign key (rc_user_id) references rc_user (id),
+    constraint fk_correction_like_correction_id foreign key (correction_id) references correction (id),
+    primary key (rc_user_id, correction_id)
+);
